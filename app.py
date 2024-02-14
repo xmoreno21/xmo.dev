2c9c50f79be082bc2a31ab48e1272006be08d4b6
Xmo <xmoreno0006@gmail.com>
2024-02-13 21:03:08 -0800

    too school for cool

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ import Config
 
 app = Flask(__name__)
 
+# /: biolink, general xmo information
+# /levelr - levelr landing page with link to docs and status pages
+# /levelr/docs - levelr documentation
+# /levelr/status - levelr status page
+# /lockr - lockr landing page with link to docs and status pages
+# /lockr/docs - lockr documentation
+# /lockr/status - lockr status page
+# /soundsworld - sw invite redirect
+
 @app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/pfp.jpg', methods = ['GET'])
+def pfp():
+    return send_file('pfp.jpg')
